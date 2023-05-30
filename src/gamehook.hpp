ec139593e140a0073572871f1f8f139d2aa67e81
SSSiyan <gsssiyan@gmail.com>
2023-05-30 15:15:36 +0100

    Update gamehook.hpp

--- src/gamehook.hpp
+++ src/gamehook.hpp
@@ -198,7 +198,7 @@ public:
 
 	static inline const char* cfgString{ "../bayo_hook.cfg" };
 	static inline utils::Config cfg{ "bayo_hook.cfg" };
-	static inline const char* dllName{"BayoHook 1.10"};
+	static inline const char* dllName{"BayoHook 1.11"};
 	static inline const char* repoUrl{ "https://github.com/SSSiyan/BayoHook" };
 private:
 };
